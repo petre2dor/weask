a8510a80df6e499a4c44aec9db2c3f8b1023435f
petre tudor <petre.2dor@gmail.com>
2013-11-11 14:02:09 +0100

    added pages via router

--- client/client.js
+++ client/client.js
@@ -1,6 +1,21 @@
 Questions = new Meteor.Collection("questions");
 
+Router.map(function() { 
+  this.route('home', {path: '/'});
+  this.route('schedule');
+  this.route('pastInterviews');
+  this.route('help');
+  this.route('about');
+
+  this.route('interview');
+});
+
+Router.configure({
+	layout: 'layout'
+});
+
 Template.newQuestions.questions = function () {
+	console.log(Questions.find({}, {sort: {date: -1}}));
   return Questions.find({}, {sort: {date: -1}});
 }
 
@@ -15,3 +30,4 @@ Template.bottomQuestions.questions = function () {
 Template.currentQuestion.questions = function () {
   return Questions.find({addressed: 'in_progress'});
 }
+
--- client/js/addQuestion-events.js
+++ client/js/addQuestion-events.js
@@ -1,12 +1,18 @@
 
 Template.addQuestion.events({
   'click button': function () {
-    var id;
+    console.log('addQuestion');
     //get counter
     Meteor.call('incCounter', 'questions', function(error, result) {
+      console.log('call incCounter');
+      console.log('error', error);
+      console.log('result', result);
       if(typeof error == 'undefined'){
         //add question
         Meteor.call('addQuestion', {text: $('#inputQuestionWrapper #questionText').val(), orderNumber: result}, function(err, res) {
+          console.log('call incCounter');
+          console.log('err', err);
+          console.log('res', res);
           if(typeof err !== 'undefined'){
             console.log(err);    
           }
